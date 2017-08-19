0980c42b4ef0b89bc2c0e04fb52c6600f4505dc9
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-19 14:09:22 -0400

    added the Met as first seed museum

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+theMet = Museum.create(name: "Metropolitan Museum of Art",
+address: "1000 5th Ave, New York, NY 10028",
+description: "The Metropolitan Museum of Art, colloquially 'the Met' is located in New York City and is the largest art museum in the United States, and is among the most visited art museums in the world. (via Wikipedia)",
+photo_url: "https://static.pexels.com/photos/69903/pexels-photo-69903.jpeg",
+link_out: "http://www.metmuseum.org/")
