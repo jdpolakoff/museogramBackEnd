eecf2b39f8cc3786ecf079d45d8c555467f8a585
ramiGA <ramisn3@gmail.com>
2017-08-21 18:45:55 -0400

    rami

--- getters.rb
+++ getters.rb
@@ -2,17 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 require 'rubygems'
 
-page = Nokogiri::HTML(open('http://naturalhistory.si.edu/exhibits'))
-page.css('.exhibit-feature').each do |info|
-  title = info.css('a').text
-title_url = page.at_css('a')['href']
-  image_url = info.at_css('img')["src"]
-    body = info.css('p').text
+page = Nokogiri::HTML(open('http://www.metmuseum.org/art/collection/'))
+page.css('.card.card--collection').each do |info|
+  title = info.at_css('a.ng-binding')
+title_url = page.css('.card__title')
+  image_url = info.at_css('img')
+    body = info.css('.card__text').text
        puts " {
                  Title: #{title}
                  title URL: #{title_url}
-                 image URL: http://naturalhistory.si.edu/exhibits/#{image_url}
-                 body: #{body}   }"
+                 image URL: #{image_url}
+                 body:      #{body}   }"
 end
 #
 # page = Nokogiri::HTML(open('https://www.instagram.com/explore/locations/378453/the-metropolitan-museum-of-art-new-york/'))
