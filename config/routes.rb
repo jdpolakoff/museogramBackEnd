df52ccb9d00d4805fb6718aa5dd1a457be033607
Liz Jewell <eghjewell@gmail.com>
2017-08-21 13:11:22 -0400

    nested review routes under artwork

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :museums do
-    resources :artworks
+    resources :artworks do
+      resources :reviews
+    end
   end
 
-  resources :reviews
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
